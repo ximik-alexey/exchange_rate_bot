fc5d45a934756c7ac192ea82fbe5ecc2953bb2a2
Ximik <ximik.87.alex@gmail.com>
2020-05-02 20:42:50 +0300

    add new funtions

--- logic.py
+++ logic.py
@@ -28,29 +28,67 @@ def _sell(arg):
         return 'rubl_sell'
 
 
-def minimum_buy(arg):
+def _exchenge(arg_1, arg_2):
     data = _load_data()
-    money = _buy(arg)
-    index = 0
-    exch = None
+    index_best = 0
+    index_loose = 0
+    exch_best = None
+    exch_loose = None
     for inx, price in enumerate(data):
-        if inx == 0:
-            exch = price[money]
-        if price[money] < exch:
-            exch = price[money]
-            index = inx
-    return data[index]['name'], exch, data[index]['website']
+        if arg_1 == 'buy':
+            money = _buy(arg_2)
+            if inx == 0:
+                exch_best = price[money]
+                exch_loose = price[money]
+            if price[money] < exch_best:
+                exch_best = price[money]
+                index_best = inx
+            if price[money] > exch_loose:
+                exch_loose = price[money]
+                index_loose = inx
+        if arg_1 == 'sell':
+            money = _sell(arg_2)
+            if inx == 0:
+                exch_best = price[money]
+                exch_loose = price[money]
+            if price[money] > exch_best:
+                exch_best = price[money]
+                index_best = inx
+            if price[money] < exch_loose:
+                exch_loose = price[money]
+                index_loose = inx
+
+    return index_best, exch_best, data, index_loose, exch_loose
 
 
-def maximum_sell(arg):
+def sell_buy(arg_1, arg_2):
+    index_best, exch_best, data, index_loose, exch_loose = _exchenge(arg_1, arg_2)
+    return data[index_best]['name'], exch_best, data[index_best]['website']
+
+
+def calc(arg_1, arg_2, arg_3):
+    index_best, exch_best, data, index_loose, exch_loose = _exchenge(arg_1, arg_3)
+    if arg_3 == 'RUB':
+        return data[index_best]['name'], (float(arg_2) * exch_best) / 100, \
+               data[index_loose]['name'], (float(arg_2) * exch_loose) / 100
+    else:
+        return data[index_best]['name'], float(arg_2) * exch_best, \
+               data[index_loose]['name'], float(arg_2) * exch_loose
+
+
+def list_sell_buy(arg_1, arg_2):
     data = _load_data()
-    money = _sell(arg)
-    index = 0
-    exch = None
-    for inx, price in enumerate(data):
-        if inx == 0:
-            exch = price[money]
-        if price[money] > exch:
-            exch = price[money]
-            index = inx
-    return data[index]['name'], exch, data[index]['website']
+    if arg_1 == 'sell':
+        sell = sorted([[i[_sell(arg_2)], i['name']] for i in data], reverse=True)
+        sell = [str(i[0]) + ' ' + str(i[1]) + '\n' for i in sell]
+        return ''.join(sell)
+    if arg_1 == 'buy':
+        buy = sorted([[i[_buy(arg_2)], i['name']] for i in data])
+        buy = [str(i[0]) + ' ' + str(i[1]) + '\n' for i in buy]
+        return ''.join(buy)
+
+
+if __name__ == '__main__':
+    arg_1 = 'buy'
+    arg_2 = 'RUB'
+    print(list_sell_buy(arg_1, arg_2))
