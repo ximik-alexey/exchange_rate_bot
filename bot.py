4d770204371d29ab080983639cd6e895c604e080
kolasdevpy <kolas.dev.py@gmail.com>
2020-05-02 15:14:30 +0300

    fix bug

--- bot.py
+++ bot.py
@@ -20,16 +20,15 @@ def send_welcome(message):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    print(message)
     try:
         n_1, n_2 = message.text.split()
         act, money = n_1.lower(), n_2.upper()
         if (act == 'sell' or act == 'buy') and (money == 'USD' or money == 'EUR' or money == 'RUB'):
             if act == 'sell':
-                kurs, bank, link = logic.maximum_sell(money)
+                bank, kurs, link = logic.maximum_sell(money)
                 bot.send_message(message.from_user.id, f' {money}: {kurs} в банке {bank}, сайт {link}')
             elif act == 'buy':
-                kurs, bank, link = logic.minimum_bay(money)
+                bank, kurs, link = logic.minimum_buy(money)
                 bot.send_message(message.from_user.id, f' {money}: {kurs} в банке {bank}, сайт {link}')
         else:
             send_welcome(message)
